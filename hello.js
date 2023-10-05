9c822fce413b6b1dac0e57a022fc0d652a7bb91b
rafalceglinski1993@gmail.com <rafal.ceglinski@makolab.com>
2023-10-05 09:52:06 +0200

    I centered navigation by flex box.

--- hello.js
+++ hello.js
@@ -1,7 +1,14 @@
 console.log('Witaj!!')
 
 
-let kontakt = document.querySelector('.footer__text--js')
+const kontakt = document.querySelector('.footer__text--js')
 
 
 kontakt.innerHTML = kontakt.innerHTML + '<br> Telefon 811 000 222'
+
+
+const hello  = (name, age) => {
+    return `Czesc, nazywam sie ` + name + ' i mam ' + age
+}
+
+console.log(hello('Rafal', 30));
